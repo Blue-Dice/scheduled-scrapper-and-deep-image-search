5f70ce82e50140595bcb2fcb4f49c961aa102a0e
Dhairya-Kumar-Sharma <dhairya.hestabit@gmail.com>
2023-03-22 17:05:42 +0530

    celery & redis fixes

--- CeleryScheduler/CeleryWorker.py
+++ CeleryScheduler/CeleryWorker.py
@@ -1,9 +1,9 @@
 from celery import Celery
+from celery.schedules import crontab
 from Helpers import Logger as Log
 from decouple import config
-import argparse
 
-class CeleryWorker():
+class CeleryAgent():
     def __init__(self) -> None:
         self.broker_uri = config('MESSAGE_BROKER_URI')
         self.app = Celery(
@@ -16,16 +16,16 @@ class CeleryWorker():
         self.app.start(['purge', '-Q', self.queues, '-f'])
     
     def celery_start(self) -> None:
-        self.app.worker_main(['worker', '--pool=threads', '-Q', self.queues, '--loglevel=DEBUG'])
+        self.app.worker_main(['worker', '-B', '--pool=threads', '-Q', self.queues, '--loglevel=INFO'])
         
-    def routes_tasks(self, routes) -> None:
+    def route_tasks(self, routes) -> None:
         try:
             self.task_routes = {}
             for route in routes:
                 self.task_routes[route[0]] = {'queue':route[1]}
             self.app.conf.update({'task_routes':self.task_routes})
             self.queues = ','.join([route[1] for route in routes])
-            Log.success('Celery tasks routes successfully registerd')
+            Log.success('Celery tasks routes registerd successfully')
         except Exception as e:
             Log.error(f'Celery task route error -> {e}')
             
@@ -38,19 +38,26 @@ class CeleryWorker():
             Log.success('Celery tasks discovery completed')
         except Exception as e:
             Log.error(f'Celery task discover error -> {e}')
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--start', action='store_true')
-parser.add_argument('--purge', action='store_true')
-
-if __name__ == '__main__':
-    agent = CeleryWorker()
-    agent.routes_tasks([
-        ['task_name','queue_name']
-    ])
-    agent.discover_tasks([
-        'folder_name'
-    ])
-    args = parser.parse_args()
-    if args.purge and not args.start: agent.celery_purge()
-    if args.start and not args.purge: agent.celery_start()
+    
+    def schedule_tasks(self) -> None:
+        try:
+            self.app.conf.beat_schedule = {
+                'Daily_task': {
+                    'task': 'Task_Scheduler',
+                    'schedule': crontab(minute=19, hour=14),
+                    'args': (['weekly']),
+                },
+                'Weekly_task': {
+                    'task': 'Task_Scheduler',
+                    'schedule': crontab(minute=0, hour=0, day_of_week='sun'),
+                    'args': (['weekly']),
+                },
+                'Monthly_task': {
+                    'task': 'Task_Scheduler',
+                    'schedule': crontab(minute=0, hour=0, day_of_month='1'),
+                    'args': (['monthly']),
+                }
+            }
+            Log.success('Celery scheduled tasks registerd successfully')
+        except Exception as e:
+            Log.error(f'Celery task schedule error -> {e}')
---
+++ scheduler.py
@@ -0,0 +1,29 @@
+import argparse
+from CeleryScheduler.CeleryWorker import CeleryAgent
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--help', action='store_true')
+parser.add_argument('--start', action='store_true')
+parser.add_argument('--purge', action='store_true')
+
+celery_help_message = """
+Celery run commands:
+--start : Start celery worker
+--purge : Purge all celery queues
+"""
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.help: print(celery_help_message)
+    else:
+        agent = CeleryAgent()
+        agent.celery_start()
+        agent.route_tasks([
+            ['task_name','queue_name']
+        ])
+        agent.discover_tasks([
+            'folder_name'
+        ])
+        agent.schedule_tasks()
+        if args.start: agent.celery_start()
+        elif args.purge: agent.celery_purge()
